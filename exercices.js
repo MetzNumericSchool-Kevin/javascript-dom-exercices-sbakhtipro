365361a3a089d16f1cdf1cab2f953a9380172b97
sbakhtipro <sarah.bakhti.pro@gmail.com>
2025-02-10 09:42:39 +0100

    ex 4

--- exercices.js
+++ exercices.js
@@ -15,6 +15,13 @@ descriptionBoutique.appendChild(nouveauP)
 document.querySelector('#blague_de_roger_le_sorcier').remove()
 
 // ------------------ ex 4 ------------------
+
+const callButton = document.querySelector('#call_archibald')
+callButton.addEventListener('click', () => {
+    alert(`J'arrive, j'arrive Aventurier!`)
+})
+
 // ------------------ ex 5 ------------------
+
 // ------------------ ex 6 ------------------
 // ------------------ ex 7 ------------------
